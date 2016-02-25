9c5ee8cea157045b5918ec0cc8d2681635ae8e24
cngraf <christopher.n.graf@gmail.com>
2016-02-24 19:00:16 -0600

    attempt to avoid recurisve call to now_with_mock_time

--- lib/groundhog_day/time_extensions.rb
+++ lib/groundhog_day/time_extensions.rb
@@ -1,7 +1,7 @@
 module GroundhogDay
   module TimeExtensions
     def now
-      if GroundhogDay.enabled?
+      if GroundhogDay.enabled?  && Date.current != GroundhogDay.date
         actual_now = super
         Time.new GroundhogDay.date.year, GroundhogDay.date.month, GroundhogDay.date.day, actual_now.hour, actual_now.min, actual_now.sec, actual_now.utc_offset
       else
